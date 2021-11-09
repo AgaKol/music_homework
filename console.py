9219558dd08960a8e2fd1c7c8fc78f77369b7371
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-11-09 20:32:59 +0000

    final tests completed

--- console.py
+++ console.py
@@ -10,16 +10,22 @@ import repositories.album_repository as album_repository
 # album1 = Album("Abbey Road", "rock", artist1)
 # album_repository.save(album1)
 
-# artist2 = Artist("Bob Dylan")
-# artist_repository.save(artist2)
+# # artist2 = Artist("Bob Dylan")
+# # artist_repository.save(artist2)
 
 # artist_repository.delete_all()
 # album_repository.delete_all()
 
-# specific = artist_repository.select_one(4)
-# print (specific.__dict__)
+# # specific = artist_repository.select_one(4)
+# # print (specific.__dict__)
 
-# for artist in artist_repository.select_all():
-#     print (artist.__dict__)
+# # for artist in artist_repository.select_all():
+# #     print (artist.__dict__)
+
+artist_repository.delete_one(11)
+album_repository.delete_one(2)
+
+# album1.genre = "Pop"
+# album_repository.update(album1)
 
 pdb.set_trace()
