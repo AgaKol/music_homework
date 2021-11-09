584f0383ec9bdf90bef032f001e2f184378739e1
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-11-09 20:01:38 +0000

    functions to update or delete album added

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -34,3 +34,13 @@ def select_all():
         album = Album(row['title'], row['genre'], artist)
         albums.append(album)
     return albums
+
+def update(album):
+    sql = "UPDATE albums SET (title, genre, artist_id) = (%s, %s, %s) WHERE id = %s"
+    values = [album.title, album.genre, album.artist.id, album.id]
+    run_sql(sql, values)
+
+def delete_one(id):
+    sql = "DELETE FROM albums WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
